9f06991e2eb7d62b9fad6c6bd662c0f82994bab4
Sonia77-art <soniasobby@gmail.com>
2022-10-10 15:02:27 +0300

    test

--- react--project/src/App.js
+++ react--project/src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Button from './Components/Button'
+import food from './food.png';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        
+      <div className="Nav">
         <Button text="Contact Us"></Button>
         <Button text="Menu"></Button>
+        <h1>The Food
+           Suppliers</h1>
         <Button text="About Us"></Button>
         <Button text="Services"></Button>
         <Button text = "Order Oline"></Button>
 
-        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
+        <h3> Are You Busy</h3>
+        <h2>DONT WORRY</h2>
+        
+
+        <img src={food} className="foodpic" alt="basket" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
